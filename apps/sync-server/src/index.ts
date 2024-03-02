e9f1e7314a5589bb313eedfaadf4203120f50424
Aluisio Amorim <42624869+Amorim33@users.noreply.github.com>
2024-03-01 21:54:17 -0300

    fix: format files with biome (#1)

--- apps/sync-server/src/index.ts
+++ apps/sync-server/src/index.ts
@@ -1,17 +1,17 @@
-import { Logger } from '@hocuspocus/extension-logger';
-import { SQLite } from '@hocuspocus/extension-sqlite';
-import { Hocuspocus } from '@hocuspocus/server';
+import { Logger } from "@hocuspocus/extension-logger";
+import { SQLite } from "@hocuspocus/extension-sqlite";
+import { Hocuspocus } from "@hocuspocus/server";
 
-import { env } from './env';
+import { env } from "./env";
 
 export const server = new Hocuspocus({
-  port: env.PORT,
-  extensions: [
-    new SQLite({
-      database: env.DATABASE_PATH,
-    }),
-    new Logger(),
-  ],
+	port: env.PORT,
+	extensions: [
+		new SQLite({
+			database: env.DATABASE_PATH,
+		}),
+		new Logger(),
+	],
 });
 
 server.listen();
