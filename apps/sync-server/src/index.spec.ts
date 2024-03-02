8d25701cce1da1b447b1a9e1633e47afb77efcde
Amorim33 <aluamorim@hotmail.com>
2024-03-01 21:49:26 -0300

    fix: format files with biome

--- apps/sync-server/src/index.spec.ts
+++ apps/sync-server/src/index.spec.ts
@@ -1,13 +1,13 @@
-import { env } from './env';
-import { server } from './index';
+import { env } from "./env";
+import { server } from "./index";
 
-it('responds with OK', async () => {
-  const response = await fetch(server.httpURL);
+it("responds with OK", async () => {
+	const response = await fetch(server.httpURL);
 
-  expect(await response.text()).toEqual('OK');
-  expect(response.status).toEqual(200);
+	expect(await response.text()).toEqual("OK");
+	expect(response.status).toEqual(200);
 });
 
-it('listens to custom port', () => {
-  expect(server.address.port).toEqual(env.PORT);
+it("listens to custom port", () => {
+	expect(server.address.port).toEqual(env.PORT);
 });
