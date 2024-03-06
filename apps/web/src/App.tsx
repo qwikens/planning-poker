634dbbfee09e5cd61d6eec5c854e140b673312fb
Julio Merisio <contatomerisio@gmail.com>
2024-03-05 22:54:27 -0300

    feat: init web package

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,29 @@
+import viteLogo from "/vite.svg";
+import "./App.css";
+import reactLogo from "./assets/react.svg";
+
+function App() {
+	return (
+		<>
+			<div>
+				<a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+					<img src={viteLogo} className="logo" alt="Vite logo" />
+				</a>
+				<a href="https://react.dev" target="_blank" rel="noreferrer">
+					<img src={reactLogo} className="logo react" alt="React logo" />
+				</a>
+			</div>
+			<h1>Vite + React</h1>
+			<div className="card">
+				<p>
+					Edit <code>src/App.tsx</code> and save to test HMR
+				</p>
+			</div>
+			<p className="read-the-docs">
+				Click on the Vite and React logos to learn more
+			</p>
+		</>
+	);
+}
+
+export default App;
