3422e85eadf1692830ab43dabe7c59b313ed0299
Julio Merisio <contatomerisio@gmail.com>
2024-03-14 10:05:08 -0300

    fix: update button and shortcuts info

--- apps/web/src/components/ui/button-rotate-border.tsx
+++ apps/web/src/components/ui/button-rotate-border.tsx
@@ -13,7 +13,7 @@ export function ButtonRotateBorder({
     >
       <div>
         <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#c2c2c2_0%,#505050_50%,#bebebe_100%)]" />
-        <span className="inline-flex items-center justify-center w-full h-full px-3 py-1 text-xs font-medium rounded-full bg-neutral-950 text-gray-50/80 backdrop-blur-3xl">
+        <span className="inline-flex items-center justify-center w-full h-full px-3 py-1 text-xs font-medium rounded-full bg-background backdrop-blur-3xl">
           {children}
         </span>
       </div>
