b8af4398c0e9f9717ca12fdcd06ba8a9636fae8b
Julio Merisio <contatomerisio@gmail.com>
2024-03-23 23:49:09 -0300

    test: shortcuts-info.tsx

--- apps/web/tests/keyboard-shortcuts.spec.ts
+++ apps/web/tests/keyboard-shortcuts.spec.ts
@@ -6,7 +6,7 @@ import { ensureIssueActive } from "./routines/ensure-issue-active";
 
 const chance = new Chance();
 
-test("keyboard shortcuts", async ({ browser }) => {
+test.only("keyboard shortcuts", async ({ browser }) => {
   const player1Name = chance.name();
   const gameName = chance.word();
 
@@ -21,26 +21,27 @@ test("keyboard shortcuts", async ({ browser }) => {
     gameName,
     userName: player1Name,
   });
+  await adminPage.getByTestId("create-issue-input").blur();
 
-  // esc close issues panel
   await adminPage.keyboard.press("Escape");
-  // remove focus
 
-  await adminPage.waitForSelector('[data-testid="issues-panel"]', {
+  await adminPage.waitForSelector('[label="issues"]', {
     state: "hidden",
   });
 
   await expect(adminPage.getByTestId("help-dialog")).toBeHidden();
 
-  // // ? open help
   await adminPage.keyboard.down("Shift");
   await adminPage.keyboard.press("?");
 
-  await expect(adminPage.getByRole("dialog")).toBeVisible();
+  await adminPage.waitForSelector('[role="dialog"]', {
+    state: "visible",
+  });
 
   await adminPage.keyboard.down("Shift");
   await adminPage.keyboard.press("?");
-  await expect(adminPage.getByTestId("help-dialog")).toBeHidden();
 
-  // // i open issues panel
+  await adminPage.waitForSelector('[role="dialog"]', {
+    state: "hidden",
+  });
 });
